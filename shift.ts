fe9590af416e43dfef9d01d2102ff4ea08824cc8
yunajoe <yunajoe@gmail.com>
2024-10-31 20:42:19 +0900

    feat: shift파일수정

--- shift.ts
+++ shift.ts
@@ -1,5 +1,3 @@
-import { data, selected } from "./utils";
-
 const duplicatedValueArr: number[] = [];
 //  배열 오름차수 정렬 함수
 const sortAscending = (arr: number[]) => {
@@ -14,36 +12,25 @@ const sortAscending = (arr: number[]) => {
   });
 };
 
-//  data와 selected array에서 같은 값을 return하는 함수
-const findSameValueArr = (dataArr: number[], selectedArr: number[]) => {
-  sortAscending(selectedArr);
-  for (let i = 0; i < dataArr.length; i++) {
-    const idx = dataArr.indexOf(selectedArr[i]);
-    if (idx !== -1) {
-      duplicatedValueArr.push(dataArr[idx]);
-    }
-  }
-
-  return duplicatedValueArr;
-};
-
 // 최종적으로 selected array를 기존의 배열에서 정렬하는 함수
 const shiftArrToLeft = (dataArr: number[], selectedArr: number[]) => {
-  let startIndex = 0;
-  const arr = findSameValueArr(dataArr, selectedArr);
+  sortAscending(selectedArr);
 
-  while (arr.length > 0) {
-    const value = arr.shift();
-    if (value) {
-      const targetIndex = dataArr.indexOf(value);
-      if (targetIndex !== -1) {
-        dataArr.splice(targetIndex, 1);
-      }
-      dataArr.splice(startIndex, 0, value);
-      startIndex++;
+  for (let i = 0; i < dataArr.length; i++) {
+    if (!selectedArr.includes(dataArr[i])) {
+      selectedArr.push(dataArr[i]);
     }
   }
+  console.log(selectedArr);
 };
 
-shiftArrToLeft(data, selected);
-console.log("data====>", data);
+shiftArrToLeft([1, 2, 3], [1]);
+shiftArrToLeft([1, 2, 3], [2]);
+shiftArrToLeft([1, 2, 3], [3]);
+shiftArrToLeft([1, 2, 3], [1, 2]);
+shiftArrToLeft([1, 2, 3], [2, 1]);
+shiftArrToLeft([1, 2, 3], [1, 3]);
+shiftArrToLeft([1, 2, 3], [3, 1]);
+shiftArrToLeft([1, 2, 3], [2, 3]);
+shiftArrToLeft([1, 2, 3], [3, 2]);
+shiftArrToLeft([1, 2, 3], [1, 2, 3]);
